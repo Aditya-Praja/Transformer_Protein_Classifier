9568ce1924eb86653960295391ad1767ca68acfb
Aditya Prajapati <ap4801@princeton.edu>
2026-07-28 18:42:31 -0400

    Multihead attention

--- src/model_attention.py
+++ src/model_attention.py
@@ -1,34 +1,66 @@
 import torch
 import torch.nn as nn
 
-class SelfAttention(nn.Module):
+class MultiHeadSelfAttention(nn.Module):
     def __init__(
         self,
         embedding_dim: int,
+        num_heads: int,
     ):
         super().__init__()
         
         self.embedding_dim = embedding_dim
+        self.num_heads = num_heads
+        self.head_dim = embedding_dim // num_heads
         
         self.query = nn.Linear(embedding_dim, embedding_dim)
         self.key = nn.Linear(embedding_dim, embedding_dim)
         self.value = nn.Linear(embedding_dim, embedding_dim)
+        
+    def split_heads(
+        self,
+        x: torch.Tensor
+    ) -> torch.Tensor:
+        
+        batch_size, sequence_length, _ = x.size()
+        
+        x = x.reshape(
+            batch_size,
+            sequence_length,
+            self.num_heads,
+            self.head_dim
+        )
+        
+        x = x.transpose(1, 2)
+        
+        return x
     
     def forward(
         self,
         x: torch.Tensor,
+        padding_mask: torch.Tensor | None = None
     ) -> torch.Tensor:
         
         Q = self.query(x)
         K = self.key(x)
         V = self.value(x)
         
+        Q = self.split_heads(Q)
+        K = self.split_heads(K)
+        V = self.split_heads(V)
+        
         attention_scores = torch.matmul(
             Q,
             K.transpose(-2, -1)
         )
         
-        attention_scores = attention_scores / (self.embedding_dim ** 0.5)
+        attention_scores = attention_scores / (self.head_dim ** 0.5)
+        
+        if padding_mask is not None:
+            attention_scores = attention_scores.masked_fill(
+                padding_mask[:, None, None, :],
+                float('-inf')
+            )
         
         attention_weights = torch.softmax(
             attention_scores,
