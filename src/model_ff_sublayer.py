a78aca640f8823567c57dc2c6d454c45a7bbcabb
Aditya Prajapati <ap4801@princeton.edu>
2026-07-29 12:27:49 -0400

    feedforward sublayer

--- src/model_ff_sublayer.py
+++ src/model_ff_sublayer.py
@@ -1,4 +1,37 @@
 import torch
 from torch import nn
 
-from 
+from src.model_feed_forward import FeedForwardNetwork
+
+class FeedForwardSublayer(nn.Module):
+    def __init__(
+        self,
+        embedding_dim: int,
+        feedforward_dim: int,
+        dropout: float = 0.1,
+    ) -> None:
+        
+        super().__init__() 
+        
+        self.FFN = FeedForwardNetwork(
+            embedding_dim=embedding_dim,
+            feedforward_dim=feedforward_dim,
+            dropout=dropout
+        )
+        
+        self.dropout = nn.Dropout(dropout)
+        
+        self.norm_layer = nn.LayerNorm(embedding_dim)
+        
+    def forward(
+        self,
+        x: torch.Tensor
+    ) -> torch.Tensor:
+        
+        ffn_output = self.FFN(x)
+        
+        ffn_output = self.dropout(ffn_output)
+        
+        output = self.norm_layer(x + ffn_output)
+        
+        return output
